027f27eb97c4dc316844af6151ccb93b7bdf2d0c
Rythmag <rythmagarwal0831@gmail.com>
2021-02-08 18:13:17 +0530

    Add two test cases

---
+++ tests/test4.c
@@ -0,0 +1,32 @@
+struct node{
+    int val;
+    struct node* next;
+}node;
+
+void printLinkedList(struct node* head){
+    if (head == NULL) {
+        return;
+    }
+    printf("%d\n", head->val);
+    printLinkedList(head->next);
+}
+
+int main() {
+    int n = 10;
+    struct node* head = NULL;// = (struct node*)malloc(sizeof(node));
+    struct node* prev = NULL;
+    for(int i = 0;i<n;i++){
+        struct node* curr = (struct node*)malloc(sizeof(node));
+        curr->val = i;
+        curr->next = NULL;  
+        if (prev == NULL) {
+            head = curr;
+            prev = curr;
+        } else {
+            prev->next = curr;
+        }
+        prev = curr;
+    }
+    printLinkedList(head);
+    return 0;
+}
