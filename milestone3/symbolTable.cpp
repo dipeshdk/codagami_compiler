4676104e23b2d9f0c0e65645139ba25b353454a1
srthkdb <srthkdb@iitk.ac.in>
2021-03-26 17:25:05 +0530

    symbol table plan

---
+++ milestone3/symbolTable.cpp
@@ -0,0 +1,48 @@
+#include<bits/stc++.h>
+#define FNode 1
+
+struct symbolTableNode {
+    int type;
+    int lineNo;
+    string name;
+    string attribute;
+    vector<string> argList; //C compatible
+}
+
+struct symbolTable {
+    map<string, struct node> symbolTable;
+    symbolTable* parent;
+    vector<symbolTable*> childList;
+}
+
+
+int insertSymbol(symbolTable* st,string type,int lineNo,string name,string attr, vector<string> argList);
+    // return 0 if successful, otherwise errorCode
+
+
+struct symbolTableNode* lookUp(symbolTable* st, string name, int &errorCode) {
+    // goes to parent if does not find in st
+    //returns node otherwise null
+}
+
+struct symbolTableNode* getNewSymbolTable(struct symbolTableNode* parent){
+    // malloc
+}
+
+struct symbolTableNode* insertSymbolTableChild(symbolTable* st){
+    //return st->childList.push_back(getNewSymbolTable(st));
+}
+
+
+
+//changes
+typedef struct ASTnode{
+	// int type;
+	int id;
+	char* name;
+	char* lexeme;
+	int isLeaf;
+	struct node* next;
+	struct node* childList;
+    struct symbolTableNode* st;
+}node;
