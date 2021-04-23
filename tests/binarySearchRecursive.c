1639d09a0a9748275ac3073929b2f3208421c43a
srthkdb <srthkdb@iitk.ac.in>
2021-04-23 17:31:26 +0530

    quicksort and binary search tested

---
+++ tests/binarySearchRecursive.c
@@ -0,0 +1,24 @@
+int binaryScr(int a[], int low, int high, int m)
+{
+    if (high >= low) {
+        int mid = low + (high - low) / 2;
+        if (a[mid] == m)
+            return mid;
+        if(a[mid] > m)
+        return binaryScr(a, low, mid - 1, m);
+        return binaryScr(a, mid + 1, high, m);
+        }
+    return -1;
+}
+
+int main(void)
+{
+    int a[5] = { 12, 13, 21, 36, 40 };
+    int i,m;
+    int n = 4;
+    int result; 
+    m=21;
+    result = binaryScr(a, 0, n - 1, m);
+    result = (result == -1) ? -2 : result+1;
+    return 0;
+}
