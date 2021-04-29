981d5af5878b95a50bce54e1aace05689d591c48
Sakshisak <sakshisa@iitk.ac.in>
2021-04-30 02:04:24 +0530

    merge main

--- tests/test1.c
+++ tests/test1.c
@@ -2,9 +2,10 @@ void printf(char *c, int d);
 
 int main() {
     char* arr;
-
-    int x = 6896849;
-    int a[3] = {0,3,8};
-    printf(arr,a[1]);
-    return 0;
+    int a,b,c;
+    a = 1; b = 30504; c  = 300;
+    if((b == c) && (b <= c)){
+        a = 160;
+    }
+    printf(arr, a);
 }
