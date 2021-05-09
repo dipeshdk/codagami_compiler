bc8dca5687e58645db6b44f22907d21106fec5a3
Chinmaya Singal <chinmayasingal@gmail.com>
2021-05-09 15:22:30 +0530

    binary search test case update

--- tests/binarySearchRecursive.c
+++ tests/binarySearchRecursive.c
@@ -1,13 +1,21 @@
+void printf(char* p, int d);
+char* str = "%d\n";
+
 int binaryScr(int a[5], int low, int high, int m)
 {
+    int mid;
     if (high >= low) {
-        int mid = low + (high - low) / 2;
-        if (a[mid] == m)
+        mid = low + (high - low) / 2;
+        /* printf(str, 12345); */
+        if (a[mid] == m){
+            /* printf(str,mid); */
             return mid;
-        if(a[mid] > m)
-        return binaryScr(a, low, mid - 1, m);
-        return binaryScr(a, mid + 1, high, m);
         }
+        if(a[mid] > m)
+            return binaryScr(a, low, mid - 1, m);
+        return binaryScr(a, mid + 1, high, m);
+    }
+    printf(str, 23456);
     return -1;
 }
 
@@ -15,10 +23,12 @@ int main(void)
 {
     int a[5] = { 12, 13, 21, 36, 40 };
     int i,m;
-    int n = 4;
+    int n = 5;
     int result; 
     m=21;
     result = binaryScr(a, 0, n - 1, m);
+    printf(str, result);
     result = (result == -1) ? -2 : result+1;
+    printf(str, result);
     return 0;
 }
