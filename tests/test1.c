1e41bcb47d7a4843602cbddffbd7f55a9652c17b
Dipesh Khandelwal <dipeshmps@gmail.com>
2021-05-11 04:18:24 +0530

    returnF working

--- tests/test1.c
+++ tests/test1.c
@@ -5,9 +5,11 @@ float f = 5.6;
  */
 /* int g = 6; */
 char* format = "%f\n";
-float fr = 90.90;
+float foo(){
+    return 45.56;
+}
 int main() {
-    float f = -fr;
+    foo();
     /* float f = -8.906453; */
     /* float pp = 66.6; */
     return 0;
