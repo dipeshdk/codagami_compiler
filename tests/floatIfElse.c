5e8376cd94caea6158e8bb6f587392e956ffefba
srthkdb <srthkdb@iitk.ac.in>
2021-05-12 00:38:39 +0530

    test case bug fix

--- tests/floatIfElse.c
+++ tests/floatIfElse.c
@@ -1,9 +1,8 @@
 void printf(char* str, double x);
-char* str = "%f\n";
+char* str = "%lf\n";
 
 int main(){
     double a = 3.0, b = 0.0, c, d= 8293.792, e = 0.00037;
-    int d = 5, e = 2;
     if((c  = (a || b))){
         printf(str, c);
         c = 10.198;
